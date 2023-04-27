69712f4ce94c64a9886652d4feba11eb01337c84
zue-hua-lee <e24094025@gs.ncku.edu.tw>
2023-04-27 21:03:37 +0800

    first

---
+++ dist/aboutus.js
@@ -0,0 +1,162 @@
+//$("#image_member1").on("tap",function(){});
+//alert("hi");
+
+//姿樺
+$("#image_member1").click(function() {
+  $(this).animate({ "opacity": "1" }, 200);
+  $("#image_member2").animate({ "opacity": "0.3" }, 200);
+  $("#image_member3").animate({ "opacity": "0.3" }, 200);
+  $("#image_member4").animate({ "opacity": "0.3" }, 200);
+  $("#image_member5").animate({ "opacity": "0.3" }, 200);
+  $(".person_word1").text("Zue-Hua Lee");
+  $(".person_word2").text("Programmer");
+  $(".person_word2").animate({right:'50vw'},0);
+  $(".person_word3").text("“Logic will get you from A to B,");
+  $(".person_word4").text("Imagination will take you everywhere.”");
+  $("#image4").css({ "background": "url(https://ppt.cc/fS2fsx@.png) no-repeat bottom left/contain" });
+
+  $("#illustration1~.illustration_word").text("台灣,新北"); //兄弟元素選擇器用~
+  $("#illustration2~.illustration_word").text("電機工程學系 - 大學部三年級");
+  $("#illustration3~.illustration_word").text("12月11日");
+
+  $(".person_word6").html("<p>我是網頁設計的新手，雖然還有很多需要學習，</p><p>但我樂於接受挑戰，對於遇到的問題會一步步地嘗試解決，</p><p>花時間研究和探索新的技術和工具，並將它們應用到自己的創作當中，</p><p>在不斷地實踐中成長，希望未來能成為優秀的網頁設計師。</p>");
+  $(".world_map").css({ "background": "url(https://ppt.cc/fGbfkx@.png) no-repeat center/contain" });
+});
+
+//昱琦
+$("#image_member2").click(function() {
+  $(this).animate({ "opacity": "1" }, 200);
+  $("#image_member1").animate({ "opacity": "0.3" }, 200);
+  $("#image_member3").animate({ "opacity": "0.3" }, 200);
+  $("#image_member4").animate({ "opacity": "0.3" }, 200);
+  $("#image_member5").animate({ "opacity": "0.3" }, 200);
+  $(".person_word1").text("Yu-Chi Hsiung");
+  $(".person_word2").text("Programmer");
+  $(".person_word2").animate({right:'46vw'},0);
+  $(".person_word3").text("““Every man dies. Not every man really lives.”");
+  $(".person_word4").text("");
+  $("#image4").css({ "background": "url(https://ppt.cc/fRRtpx@.png) no-repeat bottom left/contain" });
+
+  $("#illustration1~.illustration_word").text("台灣,台北"); //兄弟元素選擇器用~
+  $("#illustration2~.illustration_word").text("電機工程學系 - 大學部四年級");
+  $("#illustration3~.illustration_word").text("2月2日");
+
+  $(".person_word6").html("<p>我是一個富有好奇心，喜歡不斷嘗試新事物的人。</p><p>也喜歡到處旅遊，找尋各地美食並享受生活。</p><p>雖然我是網頁設計的新手，但我會盡力查找新工具來實現網頁。</p><p>目前已學過C++,Java,Python,Matlab，希望這堂課過完後可以再加上一項網頁設計。</p>");
+  $(".world_map").css({ "background": "url(https://ppt.cc/f0Mxdx@.png) no-repeat center/contain" });
+});
+
+//彩双
+$("#image_member3").click(function() {
+  $(this).animate({ "opacity": "1" }, 200);
+  $("#image_member1").animate({ "opacity": "0.3" }, 200);
+  $("#image_member2").animate({ "opacity": "0.3" }, 200);
+  $("#image_member4").animate({ "opacity": "0.3" }, 200);
+  $("#image_member5").animate({ "opacity": "0.3" }, 200);
+  $(".person_word1").text("Tsai-Shuang Lan");
+  $(".person_word2").text("Project Leader, Programmer");
+  $(".person_word2").animate({right:'40vw'},0);
+  $(".person_word3").text("“You said if I believe enough, I would fly.”");
+  $(".person_word4").text("");
+  $("#image4").css({ "background": "url(https://ppt.cc/fwUYpx@.png) no-repeat bottom left/contain" });
+
+  $("#illustration1~.illustration_word").text("台灣,基隆"); //兄弟元素選擇器用~
+  $("#illustration2~.illustration_word").text("電機工程學系 - 大學部三年級");
+  $("#illustration3~.illustration_word").text("7月30日");
+
+  $(".person_word6").html("<p>我是一個喜歡到處嘗試的人。</p><p>也喜歡大家一起熱鬧的討論、想法碰撞後再實現的過程。</p><p>不論是需要新想法的設計、或是需要不斷學習和實作新技術的寫程式，</p><p>都很有趣也值得在每一個環節認真投入，希望可以成為一個真正的team!</p>");
+  $(".world_map").css({ "background": "url(https://ppt.cc/f975Vx@.png) no-repeat center/contain" });
+});
+
+//文杰
+$("#image_member4").click(function() {
+  $(this).animate({ "opacity": "1" }, 200);
+  $("#image_member1").animate({ "opacity": "0.3" }, 200);
+  $("#image_member2").animate({ "opacity": "0.3" }, 200);
+  $("#image_member3").animate({ "opacity": "0.3" }, 200);
+  $("#image_member5").animate({ "opacity": "0.3" }, 200);
+  $(".person_word1").text("Kitson Lam");
+  $(".person_word2").text("Designer");
+  $(".person_word2").animate({right:'53vw'},0);
+  $(".person_word3").text("“Design is not just what it looks like and feels like.");
+  $(".person_word4").text("Design is how it works.”");
+  $("#image4").css({ "background": "url(https://ppt.cc/fZfAAx@.png) no-repeat bottom left/contain" });
+
+  $("#illustration1~.illustration_word").text("香港,九龍"); //兄弟元素選擇器用~
+  $("#illustration2~.illustration_word").text("工業設計系 - 大學部三年級");
+  $("#illustration3~.illustration_word").text("4月7日");
+
+  $(".person_word6").html("<p>我是一個很喜歡發問，經常會思考為什麼的人。</p><p>當發現有問題時，會馬上著手找答案。</p><p>而我認為做設計是需要勇於發問，並且接受評價才會有所進步。</p><p>我也很喜歡完成一個作品或設計時的成就感，在每一個設計的步驟我都會很認真參與，享受過程</p>");
+  $(".world_map").css({ "background": "url(https://ppt.cc/fbw9Xx@.png) no-repeat center/contain" });
+});
+
+//文謹
+$("#image_member5").click(function() {
+  $(this).animate({ "opacity": "1" }, 200);
+  $("#image_member1").animate({ "opacity": "0.3" }, 200);
+  $("#image_member2").animate({ "opacity": "0.3" }, 200);
+  $("#image_member3").animate({ "opacity": "0.3" }, 200);
+  $("#image_member4").animate({ "opacity": "0.3" }, 200);
+  $(".person_word1").text("Wen-Jin Yuan");
+  $(".person_word2").text("Programmer");
+  $(".person_word2").animate({right:'48vw'},0);
+  $(".person_word3").text("“An unexamined life is not worth living.”");
+  $(".person_word4").text("");
+  $("#image4").css({ "background": "url(https://ppt.cc/fsutzx@.png) no-repeat bottom left/contain" });
+
+  $("#illustration1~.illustration_word").text("台灣,基隆"); //兄弟元素選擇器用~
+  $("#illustration2~.illustration_word").text("電機工程學系 - 大學部三年級");
+  $("#illustration3~.illustration_word").text("10月15日");
+
+  $(".person_word6").html("<p>我是一個剛開始學習前端開發技能的新手小白。</p><p>遇到問題時，我喜歡找到答案並解決問題的快感。</p><p>而我也知道在學習網頁設計的路上，勇於發問是必須的。</p><p>我喜歡的是不斷努力學習後得到的成長，和團隊的進步以及成長後蛻變的自己。</p>");
+  $(".world_map").css({ "background": "url(https://ppt.cc/fKeTTx@.png) no-repeat center/contain" });
+});
+
+
+//floating animation
+$(document).ready(function() {
+  $('.main_word1').transition('fade up', 2000)
+  $('.main_word2').transition('fade up', 2000)
+  $('#logo').transition('fade up', 2000)
+  $('.title_word1').transition('fade up', 2500)
+  $('.title_word2').transition('fade up', 2500)
+})
+
+let display = false;
+$(window).scroll(function() {
+  var now = $(window).scrollTop() + 400;
+  var target = $('#WeLugg_aboutCompany').offset().top;
+
+  if (!display && now > target) {
+    $('.aboutCompany_word1').transition('fade up', 2000)
+    $('.aboutCompany_word2').transition('fade up', 2000)
+    $('#image_left').transition('fade up', 2000)
+    $('#image_mid').transition('fade up', 2000)
+    $('#image_right').transition('fade up', 2000)
+    $('.aboutCompany_word3').transition('fade up', 2000)
+    $('.aboutCompany_word4').transition('fade up', 2000)
+    display = true
+  }
+});
+
+  //floating animation
+  //let display=true;
+  //show when the first page is ready
+  //$(document).ready(function(){
+  //  $('.main_word1').animate({bottom:'17%', opacity:1},1500)
+  //  $('.main_word2').animate({bottom:'9%', opacity:1},1900)
+  //  $('#logo').animate({bottom:'18%', opacity:1},2300)
+  //  $('.title_word1').animate({bottom:'25%', opacity:1},2800)
+  //  $('.title_word2').animate({bottom:'25%', opacity:1},3600)
+  //  display= !display
+  //})
+
+  //show when scroll to that part
+//  $('#WeLugg_aboutCompany').mouseenter(function(){
+// $('.aboutCompany_word1').animate({top: '10%', opacity:1}, 1000)
+  //  $('.aboutCompany_word2').animate({top: '17%', opacity:1}, 1400)
+    //$('#image_left').animate({marginTop: '0%', opacity:1}, 1800)
+    //$('#image_mid').animate({marginTop: '0%', opacity:1}, 2400)
+    //display= !display
+  //})
+
+
